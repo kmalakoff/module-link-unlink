17b0ab7a9e111aed1bbdc6336c3f55067bdc0359
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 20:28:45 -0700

    Upgrade to mandatory typescript extensions

--- src/linkModule.ts
+++ src/linkModule.ts
@@ -45,7 +45,7 @@ function worker(src, nodeModules, callback) {
   });
 }
 
-import type { LinkCallback } from './types';
+import type { LinkCallback } from './types.js';
 
 export default function linkModule(src: string, nodeModules: string, callback?: undefined | LinkCallback): undefined | Promise<string> {
   if (typeof callback === 'function') return worker(src, nodeModules, callback) as undefined;
