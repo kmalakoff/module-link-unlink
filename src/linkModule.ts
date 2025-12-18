0f52b5de3c0f2ed509f7641c4b83a55193ef2ae9
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 21:23:26 -0800

    Function overloads

--- src/linkModule.ts
+++ src/linkModule.ts
@@ -47,7 +47,7 @@ function worker(src, nodeModules, callback) {
 
 import type { LinkCallback } from './types.ts';
 
-export default function linkModule(src: string, nodeModules: string, callback?: undefined | LinkCallback): undefined | Promise<string> {
-  if (typeof callback === 'function') return worker(src, nodeModules, callback) as undefined;
+export default function linkModule(src: string, nodeModules: string, callback?: LinkCallback): void | Promise<string> {
+  if (typeof callback === 'function') return worker(src, nodeModules, callback);
   return new Promise((resolve, reject) => worker(src, nodeModules, (err, restore?) => (err ? reject(err) : resolve(restore))));
 }
